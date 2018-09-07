ee9ee90324f93e7eb55ca0a31b174e9636e32f0d
Lucien Du <david.xelnaga@gmail.com>
2018-09-06 18:04:29 -0700

    Add project files.

---
+++ OpenTKTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenTKTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
